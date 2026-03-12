4e1ee12b772b3c37814e46c5644ec59e8518dabd
boongxs <davis.pilny@gmail.com>
2026-03-12 21:43:06 +0100

    added a border to search box on focus with fade in/out animation

--- lib/widgets/primary_search_bar.dart
+++ lib/widgets/primary_search_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PrimarySearchBar extends StatelessWidget {
+class PrimarySearchBar extends StatefulWidget {
   final ValueChanged<String>? onChanged;
 
   const PrimarySearchBar({
@@ -9,27 +9,65 @@ class PrimarySearchBar extends StatelessWidget {
   });
 
   @override
+  State<PrimarySearchBar> createState() => _PrimarySearchBarState();
+}
+
+class _PrimarySearchBarState extends State<PrimarySearchBar> {
+  final FocusNode _focusNode = FocusNode();
+  bool _hasFocus = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {
+        _hasFocus = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 600,
-      height: 70,
-      decoration: BoxDecoration(
-        color: const Color(0xff303030),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      alignment: Alignment.center,
-      padding: const EdgeInsets.fromLTRB(10, 0, 10, 7),
-      child: TextField(
-        style: const TextStyle(fontSize: 32, height: 1.0),
-        textAlignVertical: TextAlignVertical.center,
-        decoration: const InputDecoration(
-          border: InputBorder.none,
-          contentPadding: EdgeInsets.zero,
-          isCollapsed: true,
-          hintText: "Search...",
-          hintStyle: TextStyle(color: Colors.grey),
+    return MouseRegion(
+      cursor: SystemMouseCursors.text,
+      child: GestureDetector(
+        onTap: () => _focusNode.requestFocus(),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 100),
+          curve: Curves.easeInOut,
+          width: 600,
+          height: 70,
+          decoration: BoxDecoration(
+            color: const Color(0xff303030),
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: _hasFocus ? const Color.fromARGB(255, 117, 117, 117) : Colors.transparent,
+              width: 2,
+            ),
+          ),
+          alignment: Alignment.center,
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 7),
+          child: TextField(
+            focusNode: _focusNode,
+            style: const TextStyle(fontSize: 32, height: 1.0),
+            textAlignVertical: TextAlignVertical.center,
+            cursorColor: const Color.fromARGB(255, 161, 161, 161),
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.zero,
+              isCollapsed: true,
+              hintText: "Search...",
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+            onChanged: widget.onChanged,
+          ),
         ),
-        onChanged: onChanged,
       ),
     );
   }
